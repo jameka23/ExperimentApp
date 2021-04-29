48714b109ba85e037d117058a7dacd352afab57a
Jameka Echols <jameka.echols@gmail.com>
2021-04-28 21:22:42 -0500

    added button on storyboard

--- ExperimentApp/ViewController.swift
+++ ExperimentApp/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    // create an action
 
 }
 
